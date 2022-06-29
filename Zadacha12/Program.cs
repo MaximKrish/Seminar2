fce022b30dbded32b661653aada45d85d8c822b4
Maxim <m.krishnevskiy@kmg.kz>
2022-06-29 20:07:06 +0600

    Zadacha 12

---
+++ Zadacha12/Program.cs
@@ -0,0 +1,12 @@
+﻿int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 % n2 == 0)
+{
+    Console.WriteLine("кратно");
+    
+}
+else
+{
+     Console.WriteLine("не кратно");
+     Console.WriteLine(n1 % n2);
+}
