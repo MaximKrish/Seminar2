9432ee6562d5640af8763100a88083d911b431c7
Maxim <m.krishnevskiy@kmg.kz>
2022-06-29 19:36:42 +0600

    Zadacha 11

---
+++ Zadacha11/Program.cs
@@ -0,0 +1,9 @@
+﻿int n1, n2, n;
+n = new Random().Next(99, 1000);
+// int n = Convert.ToInt32(Console.Readline());
+Console.WriteLine(n);
+n1 = n / 100;
+n2 = n %10;
+Console.WriteLine(n1 *10 + n2);
+//Console.Write(n1);
+//Console.Write(n2);
