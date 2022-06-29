9847ac94daf105e0601902306b91aa153ee36378
Maxim <m.krishnevskiy@kmg.kz>
2022-06-29 20:32:10 +0600

    Zadacha 14

---
+++ Zadacha14/Program.cs
@@ -0,0 +1,34 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0)
+{
+    if (n % 23 == 0)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
+
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// if (n % 7 == 0 && n % 23 ==0 )
+// {
+//      Console.WriteLine("yes");
+// }
+        
+// else
+// {
+//   Console.WriteLine("no");
+// }
+
+// && условие И
+// || условие ИЛИ
