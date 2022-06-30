5b91177655e6f53630c23ab0172c0972c341c85a
Maxim <m.krishnevskiy@kmg.kz>
2022-06-30 18:00:00 +0600

    Ulitka

--- DZ13/Program.cs
+++ DZ13/Program.cs
@@ -1,2 +1,23 @@
-﻿int n, n1;
-n = Convert.ToInt32(ReadLine());
+﻿int n, n1 = 0;
+n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 100)
+{
+    n1 = n%100;
+}
+if (n > 1000)
+{
+    n1 = (n%100 - n%10)/10;
+}
+
+if (n > 10000)
+{
+    n1 = (n%1000 - n%100 -n%10)/100;
+}
+Console.WriteLine(n1);
+
+
+// int n, n1;
+// n = Convert.ToInt32(Console.ReadLine());
+// n1 = (n%100 - n%10) / 10;
+// Console.WriteLine(n1);
---
+++ Ulitka/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите высоту шеста ");
+int h = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите высоту подъема за день ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите высоту спуска за ночь ");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 0; // дни
+while (h >= 0)
+{
+    h = h - a + b;
+    i++;
+}
+Console.WriteLine("Улитка доползет до вершины за ");
+Console.WriteLine(i);
+
