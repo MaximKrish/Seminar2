2f0b71ba06c25b6b6633972662d03608b6e6cfda
Maxim <m.krishnevskiy@kmg.kz>
2022-07-01 09:07:30 +0600

    DZ13

--- DZ13/Program.cs
+++ DZ13/Program.cs
@@ -1,20 +1,56 @@
-﻿int n, n1 = 0;
+﻿int n;
 n = Convert.ToInt32(Console.ReadLine());
+if (n%100 ==n)
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа: ");
+    Console.WriteLine(Convert.ToString(n)[2]);
+}
 
-if (n > 100)
-{
-    n1 = n%100;
-}
-if (n > 1000)
-{
-    n1 = (n%100 - n%10)/10;
-}
 
-if (n > 10000)
-{
-    n1 = (n%1000 - n%100 -n%10)/100;
-}
-Console.WriteLine(n1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int n, n1 = 0;
+// n = Convert.ToInt32(Console.ReadLine());
+
+// if (n > 10)
+// {
+//     Console.WriteLine("такого числа не существует");
+// }
+// if (n > 100)
+// {
+//     n1 = n%10;
+// }
+
+// if (n > 1000)
+// {
+//     n1 = (n%100  -n%10)/10;
+// }
+
+// if (n > 10000)
+// {
+//     n1 = (n%1000  -n%100)/100;
+// }
+// Console.WriteLine(n1);
+
 
 
 // int n, n1;
